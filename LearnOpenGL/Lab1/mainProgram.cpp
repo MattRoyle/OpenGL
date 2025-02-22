563285ab1351837dedf5c7ae3c1220aa3dce46c1
MattRoyle <royle2138@gmail.com>
2025-02-22 17:01:17 +0000

    corrected normal calc

--- LearnOpenGL/Lab1/mainProgram.cpp
+++ LearnOpenGL/Lab1/mainProgram.cpp
@@ -55,7 +55,18 @@ GLfloat vertices[] = {
 	-0.5f,	 0.5f,	 0.5f,	 0.0f,	 1.0f,	 0.0f,	 0.0f,	 0.0f,	
 	-0.5f,	 0.5f,	 -0.5f,	 0.0f,	 1.0f,	 0.0f,	 0.0f,	 1.0f
 };
-
+glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+};
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 #define DISPLAY_W 640
@@ -142,7 +153,7 @@ int main()
 	glUniform1i(glGetUniformLocation(lightingShader.programID, "material.specular"), 1);
 	glUniform1f(glGetUniformLocation(lightingShader.programID, "material.shininess"),64.0f);
 
-	glUniform3fv(glGetUniformLocation(lightingShader.programID, "light.position"), 1, &lightPos[0]);
+	glUniform3fv(glGetUniformLocation(lightingShader.programID, "light.direction"), 1, &lightPos[0]);
 	glUniform3f(glGetUniformLocation(lightingShader.programID, "light.ambient"), 0.2f, 0.2f, 0.2f);
 	glUniform3f(glGetUniformLocation(lightingShader.programID, "light.diffuse"), 0.5f, 0.5f, 0.5f);
 	glUniform3f(glGetUniformLocation(lightingShader.programID, "light.specular"), 1.0f, 1.0f, 1.0f);
@@ -176,7 +187,6 @@ int main()
 
 		glUseProgram(lightingShader.programID);
 		glUniform3fv(glGetUniformLocation(lightingShader.programID, "viewPos"), 1, &camera.position[0]);
-		glUniform3fv(glGetUniformLocation(lightingShader.programID, "lightPos"), 1, &lightPos[0]);
 		// view/projection transformations
 		glfwGetWindowSize(window, &width, &height);
 		glm::mat4 projection = glm::perspective(camera.fov, (float)width / (float)height, 0.1f, 100.0f);
@@ -188,14 +198,21 @@ int main()
 		//world transformations
 		glm::mat4 model = glm::mat4(1.0f);
 		glUniformMatrix4fv(glGetUniformLocation(lightingShader.programID, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		
-		glm::mat3 normalMatrix = glm::mat3(transpose(inverse(view * model)));
-		glUniformMatrix3fv(glGetUniformLocation(lightingShader.programID, "normalMatrix"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
-
-		
+		glm::mat3 normalMatrix = glm::mat4(1.0f);
 		//render the cube
 		glBindVertexArray(VAOs[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			normalMatrix = glm::mat3(transpose(inverse(model)));
+			glUniformMatrix3fv(glGetUniformLocation(lightingShader.programID, "normalMatrix"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+			glUniformMatrix4fv(glGetUniformLocation(lightingShader.programID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		
 		//Lamp Object
 		glUseProgram(cubeLightShader.programID);
@@ -206,7 +223,7 @@ int main()
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
 		glUniformMatrix4fv(glGetUniformLocation(cubeLightShader.programID, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		normalMatrix = glm::mat3(transpose(inverse(view * model)));
+		normalMatrix = glm::mat3(transpose(inverse(model)));
 		glUniformMatrix3fv(glGetUniformLocation(lightingShader.programID, "normalMatrix"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
 
 		glBindVertexArray(VAOs[1]);
